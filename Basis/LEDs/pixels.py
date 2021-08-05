bfd24032c8ccd2cd4f8ca96777c7cc90f2bbf5b8
Nicki <nicki_bu@gmx.de>
2021-08-05 03:04:53 +0200

    New: LED Steuerung

---
+++ Basis/LEDs/pixels.py
@@ -0,0 +1,76 @@
+import math
+import threading
+import time
+
+from gpiozero import LED
+
+import apa102
+
+PINK = 0x7C1645
+LILA = 0x8F20B6
+
+class LEDSteuerung(object):
+    def __init__(self):
+        self.strip = apa102.APA102(12, 50)
+
+        self.powerOn()
+        self.clearStrip()
+
+        self.wakeup()
+        time.sleep(2)
+        self.think()
+        time.sleep(2)
+        self.speak()
+        time.sleep(2)
+        self.sleep()
+
+    def powerOn(self):
+        self.power = LED(5)
+        self.power.on()
+
+    def clearStrip(self):
+        self.strip.clear_strip()
+        self.strip.show()
+
+    def wakeup(self):
+        for x in range(30):
+            self.strip.rotate(1)
+            brightness = min(x*5, 100)
+            self.strip.set_pixel_rgb(0, PINK, bright_percent=brightness)
+            self.strip.set_pixel_rgb(6, PINK, bright_percent=brightness)
+            self.strip.show()
+            time.sleep(0.03)
+
+    def think(self):
+        for x in range(30):
+            self.strip.set_pixel_rgb(x%12, PINK)
+            self.strip.set_pixel_rgb((6+x)%12, LILA)
+            self.strip.show()
+            time.sleep(0.08)
+
+    def speak(self):
+        for x in range(6):
+            self.strip.set_pixel_rgb(x*2, PINK)
+            self.strip.show()
+            time.sleep(0.05)
+            self.strip.set_pixel_rgb(x*2+1, LILA)
+            self.strip.show()
+            time.sleep(0.05)
+
+        for x in range(900):
+            brightness = abs(math.cos(math.radians(1*x))*80)+20
+            for x in range(6):
+                self.strip.set_pixel_rgb(x*2, PINK, bright_percent=brightness)
+                self.strip.set_pixel_rgb(x*2+1, LILA, bright_percent=brightness)
+            self.strip.show()
+            time.sleep(0.01)
+
+    def sleep(self):
+        for x in range(16):
+            brightness = max(100 - x*16, 0)
+            self.strip.set_pixel_rgb(x%12, PINK, bright_percent=brightness)
+            self.strip.set_pixel_rgb((6+x)%12, LILA, bright_percent=brightness)
+            self.strip.show()
+            time.sleep(0.05)
+
+LS = LEDSteuerung()
