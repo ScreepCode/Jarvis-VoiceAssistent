56d0e16b7d6178dc75570fd1b986abadc7299e6c
Nicki <nicki_bu@gmx.de>
2021-09-25 13:40:29 +0200

    Fix: Renewed Greeting

--- Modules/Greeting.py
+++ Modules/Greeting.py
@@ -1,3 +1,10 @@
 
-def greet(args):
-    print("Hallo Niklas ^^")
+class Greeting(object):
+    def __init__(self):
+        pass
+
+    def greet(self):
+        return "Hallo Niklas wie geht es dir"
+        
+
+G = Greeting()
